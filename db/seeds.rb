b371babef46c2463c52efb622511545d30d271a2
guerric sant <guerric.sant@gmail.com>
2023-08-23 17:54:51 +0200

    TODO: css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'cleaning database'
+Flat.destroy_all
+
+puts 'Creating database...'
+
+5.times do
+  Flat.create!(
+    name: Faker::Beer.name,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentence(word_count: rand(30..100)),
+    price_per_night: rand(50..150),
+    number_of_guests: rand(2..8)
+  )
+end
